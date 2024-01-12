ab81ce1879722e8c32c095334974af8b4bf1d124
Sanphet Songjindasak <sanphetsongjindasak@gmail.com>
2024-01-12 13:46:08 +0700

    Create uploadRestaurantSubImage

--- src/utils/restaurant.js
+++ src/utils/restaurant.js
@@ -1,6 +1,6 @@
 import { supabase } from "../pages/loginsystem";
 
-// upload image to the bucket
+// upload image to the bucket for a restaurant
 export async function uploadRestaurantImage(event, restaurantName, filename){
   try {
     const file = event.target.files[0]
@@ -16,6 +16,25 @@ export async function uploadRestaurantImage(event, restaurantName, filename){
     if (error) throw new Error(error.message)
     
   } catch (error ) {
+    throw new Error(error.message)
+  }
+}
+
+// upload sub-image to the bucket for a restaurant
+export async function uploadRestaurantSubImage(event, restaurantName, filename){
+  try {
+    const file = event.target.files[0]
+    // upload to the file to the bucket
+    const {error} = await supabase
+    .storage
+    .from("Shoplist")
+    .upload(`${restaurantName}/sub/${filename.png}`, file, {
+      cacheControl: '3600',
+      upsert: false
+    })
+
+    if (error) throw new Error(error.message)
+  } catch (error) {
     throw new Error(error.message)
   }
 }
