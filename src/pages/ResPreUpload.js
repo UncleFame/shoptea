e1d54da83594e56bb04c8e4f68408a5b17b0a398
Yang Predator <newkidsth@gmail.com>
2024-01-23 18:29:48 +0700

    Create <ReviewButton /> in <ResPreUpload />

--- src/pages/ResPreUpload.js
+++ src/pages/ResPreUpload.js
@@ -1,68 +1,48 @@
 import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "./loginsystem";
+import BottomBar from "../components/BottomBar";
 
 
 const ResPreupload = () => {
-  const [imageUrl, setImageUrl] = useState(null);
   const navigate = useNavigate();
-  
-  const onReviewbottomContainerClick = useCallback(() => {
-    navigate("/upload-res");
-  }, [navigate]);
-
-  const onReviewTextClick = useCallback(() => {
-    navigate("/upload-res");
-  }, [navigate]);
-
-  const onVectorSearchIcon1Click = useCallback(() => {
-    navigate("/");
-  }, [navigate]);
-
-  const onIconClick = useCallback(() => {
-    navigate("/profile");
-  }, [navigate]);
 
   return (
-    <div className="relative bg-white w-full h-[2494px] overflow-hidden text-center text-sm text-material-theme-sys-light-primary-fixed-dim font-inter">
-      <div
-        className="absolute top-[1755ppx] left-[163px] w-[100px] h-[33px] cursor-pointer"
-        onClick={onReviewbottomContainerClick}
-      >
-        <div className="absolute top-[0px] left-[0px] rounded-[20px] box-border w-[100px] h-[33px] border-[1px] border-solid border-material-theme-sys-light-primary-fixed-dim" />
-        <div
-          className="absolute top-[9px] left-[10px] font-medium inline-block w-20 h-[15px] cursor-pointer"
-          onClick={onReviewTextClick}
-        >
-          {" "}
-          Review +
-        </div>
-      </div>
-      <div className="absolute top-[0px] left-[0px] w-[430px] h-[116px] text-base text-gray-200">
-        <div className="absolute top-[0px] left-[0px] bg-white box-border w-[430px] h-[116px] border-[1px] border-solid border-gray-300" />
-        <img
-          className="absolute top-[58px] left-[325px] w-8 h-9 object-cover cursor-pointer"
-          alt=""
-          src="/vector-search-icon-1@2x.png"
-          onClick={onVectorSearchIcon1Click}
-        />
-        <img
-          className="absolute top-[58px] left-[379px] w-[33px] h-[33px] object-cover cursor-pointer"
-          alt=""
-          src="/9131529-1@2x.png"
-          onClick={onIconClick}
-        />
-        <div className="absolute top-[67px] left-[calc(50%_-_52px)] font-extrabold inline-block w-[103px] h-[18px]">
-          Review
-        </div>
-      </div>
-      <img
-        className="absolute bottom-[-4px] left-[0px] w-[430px] h-[88px] object-cover"
-        alt=""
-        src="/barbottom@2x.png"
-      />
-    </div>
+    <main className="flex flex-col items-center gap-y-5">
+      <NavBar />
+      <ReviewButton />
+      <BottomBar />
+    </main>
   );
 };
+
+const ReviewButton = () => {
+  return (
+    <button className="bg-transparent border-2 border-solid border-green-300 w-[30%] rounded-full hover:scale-105 transition-all cursor-pointer">
+      <h1 className="text-green-300 text-sm">Review +</h1>
+    </button>
+  )
+}
+
+const NavBar = () => {
+  return (
+    <div className="w-[430px] h-[116px] text-base text-gray-200">
+      <div className="bg-white box-border w-[430px] h-[116px] border-[1px] border-solid border-gray-300" />
+      <img
+        className="absolute top-[58px] left-[325px] w-8 h-9 object-cover cursor-pointer"
+        alt=""
+        src="/vector-search-icon-1@2x.png"
+      />
+      <img
+        className="absolute top-[58px] left-[379px] w-[33px] h-[33px] object-cover cursor-pointer"
+        alt=""
+        src="/9131529-1@2x.png"
+      />
+      <div className="absolute top-[67px] left-[calc(50%_-_52px)] font-extrabold inline-block w-[103px] h-[18px]">
+        Review
+      </div>
+    </div>
+  )
+}
 
 export default ResPreupload;
