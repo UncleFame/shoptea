3ccb9ce78634d634c4dc2c424c67d388d9c34535
Yang Predator <newkidsth@gmail.com>
2024-02-21 14:21:35 +0700

    Re indent file

--- src/pages/ResPreUpload.js
+++ src/pages/ResPreUpload.js
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import BottomBar from "../components/BottomBar";
-import { useContext, useEffect, useLayoutEffect, useState,useCallback } from "react";
+import { useContext, useEffect, useLayoutEffect, useState, useCallback } from "react";
 import { deleteRestaurantById, getRestaurantsByUserId } from "../models/restaurant";
 import { ResPreUploadContext, ResPreUploadProvider } from "../contexts/ResPreUploadContext";
 import { getUserInfo } from "../models/users";
@@ -16,7 +16,7 @@ const ResPreupload = () => {
   return (
     <main className="flex flex-col overflow-x-hidden w-full items-center gap-y-5 px-5 box-border relative">
       <ResPreUploadProvider>
-        <ProfileAndSearch header="Review"/>
+        <ProfileAndSearch header="Review" />
         <DisplayRestaurant />
         <BottomBar />
       </ResPreUploadProvider>
@@ -25,18 +25,18 @@ const ResPreupload = () => {
 };
 
 const DisplayRestaurant = () => {
-  const {restaurants} = useContext(ResPreUploadContext);
-  
+  const { restaurants } = useContext(ResPreUploadContext);
+
   return (
     <div className="flex flex-col items-center gap-y-5 w-full">
-      
+
       <span className="bg-gray-300 w-full h-[2px]">  </span>
 
       <div className="flex flex-col gap-y-3 w-full px-5">
         {
-          restaurants?.map(restaurant => 
-            <RestaurantItem key={restaurant.id} restaurant={restaurant}/>
-            )
+          restaurants?.map(restaurant =>
+            <RestaurantItem key={restaurant.id} restaurant={restaurant} />
+          )
         }
       </div>
       {restaurants?.length < 1 && <ReviewButton />}
@@ -44,11 +44,11 @@ const DisplayRestaurant = () => {
   )
 }
 
-const RestaurantItem = ({restaurant}) => {
+const RestaurantItem = ({ restaurant }) => {
   const [owner, setOwner] = useState('');
-  const {user, setRestaurants} = useContext(ResPreUploadContext);
+  const { user, setRestaurants } = useContext(ResPreUploadContext);
   const navigate = useNavigate();
-  async function handleDelete(){
+  async function handleDelete() {
     try {
       await deleteRestaurantById(restaurant.id);
       await deleteRestaurantCover(restaurant.name);
@@ -64,11 +64,11 @@ const RestaurantItem = ({restaurant}) => {
   }
 
   useEffect(() => {
-    async function fetchUserInfo(){
+    async function fetchUserInfo() {
       try {
         if (user) {
           const userInfo = await getUserInfo(user.id)
-          
+
           setOwner(userInfo)
         }
       } catch (error) {
@@ -84,20 +84,20 @@ const RestaurantItem = ({restaurant}) => {
   const navigatetordit = useCallback((restaurantId) => {
     navigate(`/upload-res?method=editing&restaurantId=${restaurantId}`);
   }, [navigate]);
-  
+
   return (
     <div className="flex h-[150px] justify-between gap-x-5 w-full">
-      <img className="w-full h-full object-cover rounded-2xl" src={restaurant.imageUrl} alt="restaurant image"/>
+      <img className="w-full h-full object-cover rounded-2xl" src={restaurant.imageUrl} alt="restaurant image" />
       <div className="flex w-1/3 flex-col h-full justify-start items-start">
         <p className="w-full">{restaurant.name}</p>
         <p className="w-full break-words">{owner?.email}</p>
       </div>
       <div className="flex flex-col w-full h-full justify-between">
-      <FaRegEdit onClick={()=>navigatetordit(restaurant.id)} className=" p-2 font-semibold"
-      size={20} />
-      <RiDeleteBin6Line onClick={handleDelete} className="p-2  " size={20} />
+        <FaRegEdit onClick={() => navigatetordit(restaurant.id)} className=" p-2 font-semibold"
+          size={20} />
+        <RiDeleteBin6Line onClick={handleDelete} className="p-2  " size={20} />
 
-        
+
       </div>
     </div>
   )
@@ -106,7 +106,7 @@ const RestaurantItem = ({restaurant}) => {
 const ReviewButton = () => {
   const navigate = useNavigate();
 
-  function handleClick(e){
+  function handleClick(e) {
     e.preventDefault();
     navigate('/upload-res?method=uploading')
   }
--- src/pages/ResPreUpload.jsx
+++ src/pages/ResPreUpload.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import BottomBar from "../components/BottomBar";
-import { useContext, useEffect, useLayoutEffect, useState,useCallback } from "react";
+import { useContext, useEffect, useLayoutEffect, useState, useCallback } from "react";
 import { deleteRestaurantById, getRestaurantsByUserId } from "../models/restaurant";
 import { ResPreUploadContext, ResPreUploadProvider } from "../contexts/ResPreUploadContext";
 import { getUserInfo } from "../models/users";
@@ -19,7 +19,7 @@ const ResPreupload = () => {
   return (
     <main className="flex flex-col overflow-x-hidden w-full items-center gap-y-5 px-5 box-border relative">
       <ResPreUploadProvider>
-        <ProfileAndSearch header="Review"/>
+        <ProfileAndSearch header="Review" />
         <DisplayRestaurant />
         <BottomBar />
       </ResPreUploadProvider>
@@ -28,18 +28,18 @@ const ResPreupload = () => {
 };
 
 const DisplayRestaurant = () => {
-  const {restaurants} = useContext(ResPreUploadContext);
-  
+  const { restaurants } = useContext(ResPreUploadContext);
+
   return (
     <div className="flex flex-col items-center gap-y-5 w-full">
-      
+
       <span className="bg-gray-300 w-full h-[2px]">  </span>
 
       <div className="flex flex-col gap-y-3 w-full px-5">
         {
-          restaurants?.map(restaurant => 
-            <RestaurantItem key={restaurant.id} restaurant={restaurant}/>
-            )
+          restaurants?.map(restaurant =>
+            <RestaurantItem key={restaurant.id} restaurant={restaurant} />
+          )
         }
       </div>
       {restaurants?.length < 1 && <ReviewButton />}
@@ -47,11 +47,11 @@ const DisplayRestaurant = () => {
   )
 }
 
-const RestaurantItem = ({restaurant}) => {
+const RestaurantItem = ({ restaurant }) => {
   const [owner, setOwner] = useState('');
-  const {user, setRestaurants} = useContext(ResPreUploadContext);
+  const { user, setRestaurants } = useContext(ResPreUploadContext);
   const navigate = useNavigate();
-  async function handleDelete(){
+  async function handleDelete() {
     try {
       await deleteRestaurantById(restaurant.id);
       await deleteRestaurantCover(restaurant.name);
@@ -67,11 +67,11 @@ const RestaurantItem = ({restaurant}) => {
   }
 
   useEffect(() => {
-    async function fetchUserInfo(){
+    async function fetchUserInfo() {
       try {
         if (user) {
           const userInfo = await getUserInfo(user.id)
-          
+
           setOwner(userInfo)
         }
       } catch (error) {
@@ -87,35 +87,35 @@ const RestaurantItem = ({restaurant}) => {
   const navigatetordit = useCallback((restaurantId) => {
     navigate(`/upload-res?method=editing&restaurantId=${restaurantId}`);
   }, [navigate]);
-  
+
   return (
     <div className="flex h-[150px] box-border border-solid border-2 rounded-lg border-gray-300 justify-between gap-x-5 w-full p-[10px]">
       <div className="flex w-full gap-x-5">
 
-      <div className="flex flex-col w-1/3 h-full ">
+        <div className="flex flex-col w-1/3 h-full ">
 
-        <img className=" w-full h-5/6  object-cover rounded-2xl" src={restaurant.imageUrl} alt="restaurant image"/>
-        <div className="gap-x-1 flex flex-row text-[10px] text-gray-200 items-center h-1/6">
+          <img className=" w-full h-5/6  object-cover rounded-2xl" src={restaurant.imageUrl} alt="restaurant image" />
+          <div className="gap-x-1 flex flex-row text-[10px] text-gray-200 items-center h-1/6">
             <IoMdInformationCircle />
             <p>ข้อมูลแก้ไข วว/ดด/ปป</p>
+          </div>
         </div>
-      </div>
-      
-      <div className="flex w-1/3 flex-col h-full justify-start items-start">
-        
-        
-        <p className=" font-bold text-gray-200   w-full"> {restaurant.name}</p>
-        <div className="flex  gap-x-1 items-center h-[5px]">
-        <FaUserLarge />
-        <p className="w-full break-words text-slate-300"> {owner?.email}</p>
+
+        <div className="flex w-1/3 flex-col h-full justify-start items-start">
+
+
+          <p className=" font-bold text-gray-200   w-full"> {restaurant.name}</p>
+          <div className="flex  gap-x-1 items-center h-[5px]">
+            <FaUserLarge />
+            <p className="w-full break-words text-slate-300"> {owner?.email}</p>
+          </div>
+
         </div>
-        
-      </div>
 
       </div>
       <div className="flex flex-col w-max h-full justify-between">
-        <FaRegEdit onClick={()=>navigatetordit(restaurant.id)} className=" p-2 font-semibold"
-        size={20} />
+        <FaRegEdit onClick={() => navigatetordit(restaurant.id)} className=" p-2 font-semibold"
+          size={20} />
         <RiDeleteBin6Line onClick={handleDelete} className="p-2  text-red-600 " size={20} />
       </div>
     </div>
@@ -125,7 +125,7 @@ const RestaurantItem = ({restaurant}) => {
 const ReviewButton = () => {
   const navigate = useNavigate();
 
-  function handleClick(e){
+  function handleClick(e) {
     e.preventDefault();
     navigate('/upload-res?method=uploading')
   }
