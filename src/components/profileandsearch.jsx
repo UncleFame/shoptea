26a0c1d99330d181ff169b4f21aed1c1533945eb
Yang Predator <newkidsth@gmail.com>
2024-02-21 15:07:39 +0700

    Fix NavBar height

--- src/components/profileandsearch.jsx
+++ src/components/profileandsearch.jsx
@@ -1,36 +1,38 @@
-import React from 'react'
-import {useNavigate, useLocation} from "react-router-dom"
-import { FaSearch } from 'react-icons/fa';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { IoSearchOutline } from "react-icons/io5";
 
-export const ProfileAndSearch = ({isTitleVisible = false, header, graybar = false}) => {
+export const ProfileAndSearch = ({
+  isTitleVisible = false,
+  header,
+  graybar = false,
+}) => {
   const navigate = useNavigate();
 
   return (
-      
-        <nav className="flex relative items-center w-full justify-between gap-4 py-3 box-border px-5">
+    <nav className="flex relative items-center w-full justify-between gap-4 pt-10 pb-5 box-border px-5">
+      {isTitleVisible && (
+        <div className="flex flex-col gap-y-2">
+          <p className="m-0 text-[16px] font-bold text-gray-200">MatCha&Tea</p>
+          <p className="m-0 text-green-700 font-semibold text-[16px]">เรานำชามาให้คุณ</p>
+        </div>
+      )}
+      <p className="absolute left-1/2 translate-x-[-50%] text-gray-500 font-semibold">
+        {header}
+      </p>
+      <span
+        className={`flex gap-x-2.5 ${isTitleVisible ? "" : "ml-auto"} justify-end`}
+      >
+        <IoSearchOutline
+          className="text-black bg-gray-300 p-2.5 rounded-full"
+          size={17}
+          onClick={() => navigate("/All")}
+        />
+        <CgProfile size={35} onClick={() => navigate("/profile")} />
+      </span>
+    </nav>
+  );
+};
 
-{
-  isTitleVisible &&
-  <div className="flex flex-col ">
-  <p className="m-0 ">{`MatCha&Tea`}</p>
-  <p className="m-0 text-seagreen">เรานำชามาให้คุณ</p>
-</div>
-}
-<p className="absolute left-1/2 translate-x-[-50%] text-gray-500 font-semibold">
-  {header}
-</p>
-<span className={`flex  gap-x-4 ${isTitleVisible ? ""  : "ml-auto"} justify-end`}>
-  <IoSearchOutline className='text-black bg-gray-300 p-3 rounded-full' size={20} onClick={()=>navigate("/All")}/>
-  <CgProfile size={45} onClick={()=>navigate("/profile")}/>
-</span>
-
-
-    </nav>    
-    
-    
-  )
-}
-
-export default ProfileAndSearch
+export default ProfileAndSearch;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -7,31 +7,29 @@ import { FaFacebook } from "react-icons/fa";
 
 import { IoLogoFacebook } from "react-icons/io";
 
-
 const Profile = () => {
   const [mrETCValue, setMrETCValue] = useState("");
   const [favoriteValue, setFavoriteValue] = useState("");
   const navigate = useNavigate();
 
-  function handleSignOut(){
+  function handleSignOut() {
     try {
       signOut();
 
-      alert("ออกจากระบบสำเร็จ")
+      alert("ออกจากระบบสำเร็จ");
 
-      setTimeout(()=>{
-        window.location.href = "/"
-      }, 1000)
+      setTimeout(() => {
+        window.location.href = "/";
+      }, 1000);
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
     }
   }
 
-  function handleEdit(){
+  function handleEdit() {
     try {
-      
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
     }
   }
 
@@ -45,38 +43,45 @@ const Profile = () => {
 
   return (
     <div className="relative bg-white w-full h-ful overflow-hidden text-left  font-inter">
-          <div className="flex flex-row items-center justify-start w-f">
-              <p  className="" onClick={onBacktohome1ImageClick}><IoChevronBack />Home</p>    
-          </div>
-                  <div className="   flex-col border-solid  border-4 text-center item-center w-full justify-center gap-y-[px] ">
-                      <FaUserCircle size={80} />  
+      <div className="flex flex-row items-center justify-start w-f">
+        <p className="" onClick={onBacktohome1ImageClick}>
+          <IoChevronBack />
+          Home
+        </p>
+      </div>
+      <div className="flex-col border-solid  border-4 text-center item-center w-full justify-center gap-y-[px] ">
+        <FaUserCircle size={80} />
 
-                      <p>Username</p>
-                      <p>เเนะนำตัว</p>
-                      <div class="container py-10 mx-0 min-w-full flex flex-col items-center">
-                      <p className="text-center button-center  py-2 px-4 mt-3 rounded-lg  cursor-pointer   border-solid border-2  text-gray-200 " 
-                      onClick={onTextClick}
-                      >แก้ไข</p>
-                    </div>
+        <p>Username</p>
+        <p>เเนะนำตัว</p>
+        <div className="container py-10 mx-0 min-w-full flex flex-col items-center">
+          <p
+            className="text-center button-center  py-2 px-4 mt-3 rounded-lg  cursor-pointer   border-solid border-2  text-gray-200 "
+            onClick={onTextClick}
+          >
+            แก้ไข
+          </p>
+        </div>
 
-                      
-                      <div className="flex flex-row items-center mt-4">
-                      <IoLogoFacebook className="text-blue-500"  size={50}/>
+        <div className="flex flex-row items-center mt-4">
+          <IoLogoFacebook className="text-blue-500" size={50} />
 
-                        <p className="text-gray-200">facebook/profile</p>
-                      </div>
-                     
-                  </div>
-                  
-                        <div className="  w-full bg-white  box-border mt-4  h-[2px] gap-y-5 border-solid border-gray-400" />
-                        <div className="   w-full bg-darkgray box-border h-[2px] gap-y-5 border-solid border-gray-400" />
+          <p className="text-gray-200">facebook/profile</p>
+        </div>
+      </div>
 
-                    <div class="w-full flex flex-col items-center px-4 mt-4]">
-                    <p className=" rounded-lg mx-auto w-[450px]  cursor-pointer text-center border-solid border-2 text-gray-200 " 
-                    onClick={handleSignOut}>ออกระบบ</p>
+      <div className="  w-full bg-white  box-border mt-4  h-[2px] gap-y-5 border-solid border-gray-400" />
+      <div className="   w-full bg-darkgray box-border h-[2px] gap-y-5 border-solid border-gray-400" />
 
-                    </div>
-     </div>
+      <div className="w-full flex flex-col items-center px-4 mt-4]">
+        <p
+          className=" rounded-lg mx-auto w-[450px]  cursor-pointer text-center border-solid border-2 text-gray-200 "
+          onClick={handleSignOut}
+        >
+          ออกระบบ
+        </p>
+      </div>
+    </div>
   );
 };
 
