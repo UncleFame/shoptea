8578305177aad92f2d36b901616d8c9616cf591a
Yang Predator <newkidsth@gmail.com>
2024-02-21 16:31:49 +0700

    Fix layout in <ResPreUpload />

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   useLocation,
 } from "react-router-dom";
 import All from "./pages/All";
-import ResPreupload from "./pages/ResPreUpload";
+import ResPreupload from "./pages/ResPreUpload.jsx";
 import UploadRes from "./pages/UploadRes";
 import LandingPage from "./pages/LandingPage";
 import Register from "./pages/Register";
