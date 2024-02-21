a41060dc0d13eb50124ce37ae4883846f9c047ed
Yang Predator <newkidsth@gmail.com>
2024-02-21 15:57:26 +0700

    Fix <RestaurantItem /> font size

--- src/components/destination/Star.jsx
+++ src/components/destination/Star.jsx
@@ -1,18 +1,20 @@
-import React, { FC } from 'react'
-import {FaStar} from "react-icons/fa"
+import React, { FC } from "react";
+import { FaStar } from "react-icons/fa";
 
-export const Star = ({rating, textColor = "text-gray-500"}) => {
+export const Star = ({ rating, textColor = "text-gray-500" }) => {
   const isNumber = Math.floor(rating) === rating;
   return (
-    <div className='flex items-center justify-start gap-x-2 h-[30px]'>
-      <ul className='flex p-0 m-0 gap-x-1'>
-        {
-          Array.from({length : rating}, (_, index)=>{
-            return <FaStar key={index} className='text-yellow-400'/>
-          })
-        }
-      </ul>
-      <p className={`${textColor ? textColor : "text-gray-300"} text-sm`}>{isNumber ? `${rating}.0` : rating}</p>
+    <div className="flex items-center justify-start gap-x-2 h-[20px]">
+      {rating > 0 && (
+        <ul className="flex p-0 m-0 gap-x-1">
+          {Array.from({ length: rating }, (_, index) => {
+            return <FaStar key={index} className="text-yellow-400" />;
+          })}
+        </ul>
+      )}
+      <p className={`${textColor ? textColor : "text-gray-300"} text-sm font-semibold`}>
+        {isNumber ? `${rating}.0` : rating}
+      </p>
     </div>
-  )
-}
+  );
+};
--- src/pages/LandingPageForReal.jsx
+++ src/pages/LandingPageForReal.jsx
@@ -88,11 +88,11 @@ const RestaurantItem = ({ restaurant }) => {
         onClick={() => navigate(`/Review?restaurantId=${restaurant.id}`)}
         alt={restaurant.name}
       />
-      <div className="flex flex-col rounded">
-        <p className="m-0 text-gray-950">{restaurant.name}</p>
+      <div className="flex flex-col gap-y-[1px]">
+        <p className="m-0 text-gray-500 font-bold text-lg">{restaurant.name}</p>
         <Star rating={restaurant.star} />
-        <p className="m-0 text-sm text-gray-100 flex flex-row gap-2">
-          <span className="text-black">Open </span> {restaurant.open} -{" "}
+        <p className="m-0 text-sm text-gray-500 font-semibold flex flex-row gap-2">
+          <span className="text-gray-200 font-semibold text-sm">Open: </span> {restaurant.open} -{" "}
           {restaurant.close}
         </p>
         <p className="m-0 text-sm text-gray-100 ">{restaurant.name}</p>
