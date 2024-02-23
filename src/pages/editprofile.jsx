89e5e43724efd6e473c455116c82266be286313c
Sanphet Songjindasak <sanphetsongjindasak@gmail.com>
2024-02-23 15:56:11 +0700

    Able to edit profile

--- src/pages/editprofile.jsx
+++ src/pages/editprofile.jsx
@@ -1,34 +1,93 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { IoIosArrowBack } from "react-icons/io";
-import { CgProfile } from "react-icons/cg";
 import { useNavigate } from "react-router-dom";
 import { ProfileImage } from "../components/profileandsearch";
 import { useUser } from "../hooks/useUser";
-import { useFetchUser } from "../hooks/useFetchUser";
-import { getUserInfo } from "../models/users";
+import { getUserInfo, updateUser } from "../models/users";
+
+const EditProfileContext = createContext();
 
 const Editprofile = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    introduction: "",
+    faceBookUrl: "",
+  });
+  const { user } = useUser();
+  const { email, introduction, faceBookUrl } = formData;
+
+  function handleUpdateFormData(e) {
+    setFormData((_) => ({
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  async function handleUpdateUser() {
+    try {
+      await updateUser(user.id, {
+        email,
+        introduction,
+        faceBookUrl,
+      });
+      alert("แก้ไขข้อมูลสำเร็จ");
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  useEffect(() => {
+    if (!user) return;
+    const fetchUser = async () => {
+      const fetchedUser = await getUserInfo(user.id);
+      console.log(fetchedUser);
+      setFormData((_) => ({
+        email: fetchedUser.email,
+        introduction: fetchedUser.introduction,
+        faceBookUrl: fetchedUser.faceBookUrl,
+      }));
+    };
+
+    fetchUser();
+  }, [user]);
   const Navigte = useNavigate();
   const Goeditpage = useCallback(() => {
     Navigte("/Profile");
   }, [Navigte]);
 
   return (
-    <div className="relative bg-white w-full h-ful overflow-hidden text-left  font-sans">
-      <NavBar Goeditpage={Goeditpage} />
-      <Content />
-    </div>
+    <EditProfileContext.Provider
+      value={{
+        email,
+        introduction,
+        faceBookUrl,
+        handleUpdateFormData,
+        handleUpdateUser
+      }}
+    >
+      <div className="relative bg-white w-full h-ful overflow-hidden text-left  font-sans">
+        <NavBar Goeditpage={Goeditpage} />
+        <Content />
+      </div>
+    </EditProfileContext.Provider>
   );
 };
 
 function NavBar({ Goeditpage }) {
+  const {handleUpdateUser} = useContext(EditProfileContext);
   return (
     <div className="flex flex-row items-center justify-between w-[90%] mx-auto font-semibold">
       <p className="flex items-center text-gray-200" onClick={Goeditpage}>
         <IoIosArrowBack />
         Profile
       </p>
-      <p className="text-green-400" onClick={Goeditpage}>
+      <p
+        className="text-green-400" onClick={handleUpdateUser}>
         {" "}
         บันทึก
       </p>
@@ -46,34 +105,25 @@ function Content({}) {
 }
 
 function Form({}) {
-  const [formData, setFormData] = useState({
-    email: "",
-    introduction: "",
-    faceBookUrl: "",
-  });
-  const { user } = useUser();
-  const { email, introduction, faceBookUrl } = formData;
-
-  useEffect(() => {
-    if (!user) return;
-    const fetchUser = async () => {
-      const fetchedUser = await getUserInfo(user.id);
-      console.log(fetchedUser)
-      setFormData((_) => ({
-        email: fetchedUser.email,
-        introduction: fetchedUser.introduction,
-        faceBookUrl : fetchedUser.faceBookUrl
-      }));
-    };
-
-    fetchUser();
-  }, [user]);
-
+  const { email, introduction, faceBookUrl, handleUpdateFormData } = useContext(EditProfileContext);
   return (
     <form class="container py-10 w-11/12 flex flex-col gap-y-2 px-4">
-      <Input value={email} label="ชื่อ" placeholder="ชื่อ" />
-      <TextArea value={introduction} label="แนะนำตัว" />
       <Input
+        onChange={handleUpdateFormData}
+        name="email"
+        value={email}
+        label="ชื่อ"
+        placeholder="ชื่อ"
+      />
+      <TextArea
+        onChange={handleUpdateFormData}
+        name="introduction"
+        value={introduction}
+        label="แนะนำตัว"
+      />
+      <Input
+        onChange={handleUpdateFormData}
+        name="faceBookUrl"
         value={faceBookUrl}
         label="URL Facebook"
         placeholder="Url facebook"
@@ -84,14 +134,16 @@ function Form({}) {
 
 export default Editprofile;
 
-function Input({ label, placeholder, value }) {
+function Input({ label, placeholder, value, name, onChange }) {
   return (
     <>
       <div>
         <p className="w-full text-start p-0 m-0">{label}</p>
       </div>
       <input
+        onChange={onChange}
         value={value}
+        name={name}
         type="text"
         placeholder={placeholder}
         className="rounded-lg border-gray-300 border-solid border-2 h-4 p-2"
@@ -100,11 +152,13 @@ function Input({ label, placeholder, value }) {
   );
 }
 
-function TextArea({ label, value }) {
+function TextArea({ label, value, name, onChange }) {
   return (
     <>
       <p className="w-full text-start p-0 m-0">{label}</p>
       <textarea
+        onChange={onChange}
+        name={name}
         value={value}
         type="text"
         placeholder=""
