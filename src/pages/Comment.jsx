fd36534ca03eb1c0091ccd07fd1c2cdb084ae68d
Sanphet Songjindasak <sanphetsongjindasak@gmail.com>
2024-02-24 12:04:09 +0700

    Able to show current user in <Comment />

--- src/pages/Comment.jsx
+++ src/pages/Comment.jsx
@@ -1,60 +1,64 @@
 import { useEffect, useRef, useState } from "react";
-import { getPublicUrl, uploadRestaurantImage } from "../utils/restaurant";
-import { useCallback } from "react";
-import { useNavigate } from "react-router-dom";
-import { getRestaurantInfoById, insertNewRestaurant, updateRestaurant } from "../models/restaurant";
-import {useUser} from "../hooks/useUser"
-import { useSearchParams } from "react-router-dom";
-
-
+import { useUser } from "../hooks/useUser";
+import { ProfileImage } from "../components/profileandsearch";
+import { getProfilePublicUrl } from "../models/storage.ts";
 
 const Comment = () => {
-  const [comment,setComment] = useState("")
-  const handleUploadImage = () => {
+  const [formData, setFormData] = useState({
+    comment: "",
+    rating: 0,
+  });
+  const { comment, rating } = formData;
 
-  }
+  const handleUploadImage = () => {};
 
-    return ( 
-  <div className="relative bg-white w-full h-[2494px] overflow-hidden text-left text-base text-gray-200 font-inter">
-    <div className="flex w-full justify-between mt-50 px-5 box-border font-bold">
-      <button  className="text-material-theme-sys-dark-error-container bg-transparent text-left inline-block">
-        <p>ยกเลิก </p>
-      </button>
-      <p>rest</p>
-      <button onClick={handleUploadImage} className="text-material-theme-sys-light-primary-fixed text-left inline-block bg-transparent">
-      สร้าง 
-      </button>
-    </div>
-     
-
-      <div>
-      </div>
-      <div>
-        
-
+  return (
+    <div className="relative bg-white w-full h-[2494px] overflow-hidden text-left text-base text-gray-200 font-inter">
+      <div className="flex w-full justify-between mt-50 px-5 box-border font-bold">
+        <button className="text-material-theme-sys-dark-error-container bg-transparent text-left inline-block">
+          <p>ยกเลิก </p>
+        </button>
+        <p>rest</p>
+        <button
+          onClick={handleUploadImage}
+          className="text-material-theme-sys-light-primary-fixed text-left inline-block bg-transparent"
+        >
+          สร้าง
+        </button>
       </div>
 
-    <div className="flex flex-col">
-        <div className="flex flex">
-        <img
-        className="rounded-[50%] w-[37px] h-[37px] object-cover"
-        alt=""
-        src="/ellipse-4@2x.png"
-      />
-      <p>Annette Black</p>
-    
-        </div>
-        
-      <input
-        className="mx-8 rounded-lg"
-        value={comment}
-        onChange={e => setComment(e.target.value)}
-        placeholder="comment"
-      />
+      <div className="flex flex-col">
+        <ProfileCard />
 
+        <input
+          className="mx-8 rounded-lg"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          placeholder="comment"
+        />
+      </div>
     </div>
-        </div>
-  
+  );
+};
+export default Comment;
+
+function ProfileCard({}) {
+  const {user} = useUser();
+  const [imgSrc, setImgSrc] = useState("");
+
+  useEffect(()=>{
+    if (!user) return 
+    const fetchProfileImage = () => {
+      const fetchedProfileImage = getProfilePublicUrl(user.id)
+      setImgSrc(fetchedProfileImage);
+    }
+
+    fetchProfileImage()
+  }, [user])
+  return (
+    <div className="flex gap-x-2">
+      <ProfileImage imgSrc={imgSrc} size={50}/>
+      <p className="font-semibold">{user?.email}</p>
+    </div>
   );
 }
-export default Comment
